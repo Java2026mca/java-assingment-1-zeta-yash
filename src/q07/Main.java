ed0d55c01a932cadf9c1cbb54d7757b352702fc4
Yash Gupta <somgupta070703@gmail.com>
2026-04-07 22:30:41 +0530

    Refactor bubble sort to eliminate unused variable
    Removed unnecessary swapped flag in bubble sort implementation.

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -13,19 +13,14 @@ public class Main {
         int swaps = 0;
 
         for (int i = 0; i < n - 1; i++) {
-            boolean swapped = false;
-
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swaps++;
-                    swapped = true;
                 }
             }
-
-            if (!swapped) break;
         }
 
         for (int i = 0; i < n; i++) {
