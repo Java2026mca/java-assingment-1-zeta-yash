404203e26ca4a07c9133d44855220ad790eb2dc1
Yash Gupta <somgupta070703@gmail.com>
2026-04-07 12:47:29 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,21 +5,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
+        // Upper half of hollow diamond
+        for (int i = 1; i <= n; i++) {
 
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print hollow pattern
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*"); // border
+                } else {
+                    System.out.print(" "); // hollow space
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower half of hollow diamond
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print hollow pattern
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*"); // border
+                } else {
+                    System.out.print(" "); // hollow space
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
